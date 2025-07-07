e0a8f0ca0b273374efdb45a3052e3e8a12863734
tanmmayyy <jaintanmay543@gmail.com>
2025-07-08 01:53:01 +0530

    exception.py

---
+++ src/exception.py
@@ -0,0 +1,8 @@
+import sys
+
+def error_message_detail(error , error_deatil:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_messaage = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format()
+    file_name, exc_tb.tb_lineno, str(error)
+    
